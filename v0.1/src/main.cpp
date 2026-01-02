4cb1fe8bf695f9ced3f80f5345afca5d41615297
BrigitaVT <brigitaalenskaite@gmail.com>
2026-01-02 12:47:02 +0200

    Update main.cpp

--- v0.1/src/main.cpp
+++ v0.1/src/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include "Studentas.h"
 
 int main() {
-    std::cout << "SD1 v0.1 - programa veikia" << std::endl;
+    try {
+        Studentas s;
+
+        s.setVardas("Jonas");
+        s.setPavarde("Jonaitis");
+
+        s.addND(8);
+        s.addND(9);
+        s.addND(10);
+
+        s.setEgzaminas(9);
+
+        std::cout << "Studentas: "
+                  << s.getVardas() << " "
+                  << s.getPavarde() << std::endl;
+
+        std::cout << "Galutinis balas (vidurkis): "
+                  << s.galutinisVidurkis() << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Klaida: " << e.what() << std::endl;
+    }
+
     return 0;
 }
