ea59da3af8f6c5d039826825da3f5400e2e13e55
BrigitaVT <brigitaalenskaite@gmail.com>
2026-01-02 12:43:58 +0200

    Create Studentas.h

---
+++ v0.1/include/Studentas.h
@@ -0,0 +1,32 @@
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
+
+#include <string>
+#include <vector>
+
+class Studentas {
+private:
+    std::string vardas;
+    std::string pavarde;
+    std::vector<int> nd;   // namų darbų pažymiai
+    int egzaminas;
+
+public:
+    // Konstruktorius
+    Studentas();
+
+    // Set'eriai
+    void setVardas(const std::string& v);
+    void setPavarde(const std::string& p);
+    void addND(int pazymys);
+    void setEgzaminas(int egz);
+
+    // Get'eriai
+    std::string getVardas() const;
+    std::string getPavarde() const;
+
+    // Skaičiavimai
+    double galutinisVidurkis() const;
+};
+
+#endif
